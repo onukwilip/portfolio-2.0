c71ef920d0165bd8887d47f80d16af93a290eced
onukwilip <onukwilip@gmail.com>
2023-07-29 19:00:47 +0100

    Finished implementing the brain svg section

--- src/types.ts
+++ src/types.ts
@@ -12,3 +12,32 @@ export type ModelType = {
   props: JSX.IntrinsicElements["group"];
 };
 export type Stacktype = "frontend" | "backend" | "database";
+export type Skilltype =
+  | "HTML 5"
+  | "CSS 3"
+  | "Semantic UI"
+  | "Bootstrap"
+  | "SCSS"
+  | "Javascript"
+  | "Typescript"
+  | "Webpack"
+  | "Three Js"
+  | "React Js"
+  | "Next Js"
+  | "Node Js"
+  | "C#"
+  | "ASP.NET"
+  | "Microsoft SQL"
+  | "Mongo DB"
+  | "Redis"
+  | "Firebase"
+  | "Git"
+  | "GitHub"
+  | "Docker";
+export type SelectedSkillType =
+  | {
+      stack: Stacktype;
+      skill?: Skilltype;
+      // | string;
+    }
+  | undefined;
